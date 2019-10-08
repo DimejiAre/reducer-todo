f09e9d3bb0d5a43ace4830b08dec8a328b11c2a9
DimejiAre <olardi2@gmail.com>
2019-10-08 13:34:39 +0100

    add addTodo form and function

--- reducer-todo/src/components/Todos.jsx
+++ reducer-todo/src/components/Todos.jsx
@@ -1,12 +1,32 @@
-import React, {useReducer} from 'react';
+import React, {useState,useReducer} from 'react';
 import { reducer, initialState } from '../reducers/reducer';
+
+const ADD_TODO = 'ADD_TODO';
 
 function Todos(){
 
-    const [todos, dispatch] = useReducer(reducer, initialState)
+    const [todos, dispatch] = useReducer(reducer, initialState);
+    const [formValue, setFormValue] = useState('');
+
+    const addTodo = event => {
+        event.preventDefault();
+        dispatch({
+            type: ADD_TODO,
+            payload: {item: formValue}
+        })
+        setFormValue('');
+    }
+
+    const onChange = e => {
+        setFormValue(e.target.value)
+    }
 
     return (
         <div>
+            <form>
+                <input value={formValue} onChange={onChange} type='text' />
+                <button onClick={addTodo} type='submit'>Enter</button>
+            </form>
             <ul>
             {
                 todos?
