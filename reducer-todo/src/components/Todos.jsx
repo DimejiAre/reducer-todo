171b5e0124a90b2c7f1e142bbd01f324ae456263
DimejiAre <olardi2@gmail.com>
2019-10-08 14:20:18 +0100

    add markComplete and clearCompleted functionality to app

--- reducer-todo/src/components/Todos.jsx
+++ reducer-todo/src/components/Todos.jsx
@@ -2,6 +2,8 @@ import React, {useState,useReducer} from 'react';
 import { reducer, initialState } from '../reducers/reducer';
 
 const ADD_TODO = 'ADD_TODO';
+const MARK_COMPLETE = 'MARK_COMPLETE';
+const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
 
 function Todos(){
 
@@ -17,6 +19,21 @@ function Todos(){
         setFormValue('');
     }
 
+    const markComplete = id => event => {
+        event.preventDefault();
+        dispatch({
+            type: MARK_COMPLETE,
+            payload: {item_id: id}
+        })
+    }
+
+    const clearCompleted = event => {
+        event.preventDefault();
+        dispatch({
+            type: CLEAR_COMPLETED
+        })
+    }
+
     const onChange = e => {
         setFormValue(e.target.value)
     }
@@ -27,11 +44,12 @@ function Todos(){
                 <input value={formValue} onChange={onChange} type='text' />
                 <button onClick={addTodo} type='submit'>Enter</button>
             </form>
+            <button onClick={clearCompleted}>Clear Completed</button>
             <ul>
             {
                 todos?
                 todos.map(todo => (
-                    <li key={todo.id}>{todo.item}</li>
+                    <li key={todo.id} onClick={markComplete(todo.id)}>{todo.item}</li>
                 ))
                 : null
             }
--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -13,7 +13,22 @@ const initialState = [{
 function reducer (state, action){
     switch(action.type){
         case 'ADD_TODO':
-            return [...state, {item: action.payload.item, completed: 'false', id: new Date()}]
+            if(action.payload.item){
+                return [...state, {item: action.payload.item, completed: false, id: new Date()}]
+            }
+        case 'MARK_COMPLETE':
+            return state.map(todo => {
+                if(todo.id === action.payload.item_id){
+                    return {item: todo.item, completed: !todo.completed, id: todo.id}
+                }
+                return todo
+            })
+        case 'CLEAR_COMPLETED':
+            return state.filter(todo => {
+                if(todo.completed === false){
+                    return todo
+                }
+            })
         default:
             return state;
     }
